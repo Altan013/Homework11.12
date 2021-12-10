4d9227d3400e19c8ca849d00b7bf103120deec8c
Altan A. Ibrahimli <altanai@code.local>
2021-12-10 16:07:20 +0400

    2nin quvveti

---
+++ ConsoleApp2/ConsoleApp2/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace homework1
+{
+    public class Quvvet
+    {
+        public void ikininquvveti(int num)
+        {
+            if (num == 0)
+            {
+                Console.WriteLine("0");
+            }
+            while (num % 2 == 0)
+            {
+                num = num / 2;
+            }
+            if (num == 1)
+            {
+                Console.WriteLine("2nin quvvetidir");
+            }
+            else
+            {
+                Console.WriteLine("2nin quvveti deyil");
+            }
+
+        }
+    }
+
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using homework1;
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Quvvet q = new Quvvet();
+            q.ikininquvveti(128);
+        }
+    }
+}
