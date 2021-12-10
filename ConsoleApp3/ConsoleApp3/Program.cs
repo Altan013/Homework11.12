cd6398d51b076a328e9582ea260f8c4c16a5c61c
Altan A. Ibrahimli <altanai@code.local>
2021-12-10 16:56:46 +0400

    sozun sayinin tapilmasi

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task4 Total number of words in the string
+            //String word = "This is Code Academy";
+            //int count = 0;
+            //string[] arr = word.Split(" ");
+            //foreach (string item in arr)
+            //{
+            //    if (item != "")
+            //    {
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine(count);
+
+
+            #endregion
+
+            #region Task5 The frequency of the word "the"
+            //String Sentence = "The string where the word the present more than once";
+            //string[] sozarr = Sentence.Split(" ");
+            //string search = "the";
+            //int count = 0;
+
+            //foreach (var item in sozarr)
+            //{
+            //    if (item.ToLower() == search) 
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+            #endregion
+
+            #region MyRegion
+
+
+
+            #endregion
+
+        }
+    }
+}
