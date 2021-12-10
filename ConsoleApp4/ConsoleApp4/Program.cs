773a6b3521b6a0b49f8f1d75be8c8fc8a30b9685
Altan A. Ibrahimli <altanai@code.local>
2021-12-10 17:43:57 +0400

    array tasklari student

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu1 = new Student("Altan Ibrahimli", 1, "Junior Developer", "Code Academy", "altanai@code.edu.az", 0513834993);
+            stu1.Information();
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task2 10 elementi sondan evvele duzmek
+
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //for (int i = arr.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+
+            #endregion
+
+
+            #region task3 2 arraydan 1 array yaratmaq
+
+
+            //int[] arr = { 5, 3, 4 };
+            //int[] arr1 = { 6, 8, 2 };
+            //int[] arr2 = new int[arr.Length+arr1.Length];
+            //for (int i = 0, a=0;  i < arr2.Length; i++)
+            //  {
+            //    if (i<arr.Length)
+            //    {
+            //        arr2[i] = arr[i];
+            //  }
+            //    if (i==arr.Length || i>arr.Length)
+            //    {
+            //        arr2[i] = arr1[a];
+            //        a++;
+            //    }
+
+            //}
+            //foreach (var item in arr2)
+            //{
+
+            //}
+
+            //Array.Sort(arr2);
+            //Array.Reverse(arr2);
+            //foreach (int item in arr2)
+            //{
+
+            //}
+            //Console.WriteLine(string.Join(" ", arr2));
+
+            #endregion
+
+
+
+            #region task4 min ve max element
+
+
+
+
+            //int[] arr = { 2, 25, 88, 157 };
+            //int min=arr[0];
+            //int max=arr[0];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i]>max) 
+            //    {
+            //        max = arr[i];
+            //    }
+            //    else if (min>arr[i])
+            //    {
+            //        min = arr[i];
+            //    }
+            //}
+            //quvvet
+            //Console.WriteLine(min);
+            //Console.WriteLine(max);
+            //2nin quvveti
+            //Quvvet num1 = new Quvvet();
+            //num1.Quvvet2(256);
+
+            #endregion
+
+
+
+        }
+    }
+}
